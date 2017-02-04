0a52a7e8292180a00c03d513a34dc59dffff8258
WhiteZane <whi12059@byui.edu>
2017-02-04 12:27:01 -0700

    first commit

---
+++ MegaEscarito/MegaEscarito/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace MegaEscarito
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.drawers = new System.Windows.Forms.NumericUpDown();
+            this.width = new System.Windows.Forms.TextBox();
+            this.length = new System.Windows.Forms.TextBox();
+            this.rush = new System.Windows.Forms.Label();
+            this.days = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drawers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desk Order Application";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Width in inches";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Length in inches";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Amount of drawers";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(131, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Material";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(134, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(82, 56);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // drawers
+            // 
+            this.drawers.Location = new System.Drawing.Point(15, 108);
+            this.drawers.Name = "drawers";
+            this.drawers.Size = new System.Drawing.Size(92, 20);
+            this.drawers.TabIndex = 6;
+            // 
+            // width
+            // 
+            this.width.Location = new System.Drawing.Point(12, 42);
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(80, 20);
+            this.width.TabIndex = 7;
+            // 
+            // length
+            // 
+            this.length.Location = new System.Drawing.Point(134, 42);
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(82, 20);
+            this.length.TabIndex = 8;
+            // 
+            // rush
+            // 
+            this.rush.AutoSize = true;
+            this.rush.Location = new System.Drawing.Point(13, 144);
+            this.rush.Name = "rush";
+            this.rush.Size = new System.Drawing.Size(35, 13);
+            this.rush.TabIndex = 9;
+            this.rush.Text = "label6";
+            // 
+            // days
+            // 
+            this.days.Location = new System.Drawing.Point(15, 244);
+            this.days.Name = "days";
+            this.days.Size = new System.Drawing.Size(79, 20);
+            this.days.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(100, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Days to Rush";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(187, 241);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 12;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(259, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Out put order";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 286);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.days);
+            this.Controls.Add(this.rush);
+            this.Controls.Add(this.length);
+            this.Controls.Add(this.width);
+            this.Controls.Add(this.drawers);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mega Escritorio";
+            ((System.ComponentModel.ISupportInitialize)(this.drawers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown drawers;
+        private System.Windows.Forms.TextBox width;
+        private System.Windows.Forms.TextBox length;
+        private System.Windows.Forms.Label rush;
+        private System.Windows.Forms.TextBox days;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ MegaEscarito/MegaEscarito/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaEscarito
+{
+    public enum Material {Oak, Pine, Laminate, Purpleheart, Zebrawood, Mahogany };
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Array valArray = Enum.GetValues(typeof(Material));
+
+            foreach (Material materialValue in valArray)
+            {
+                listBox1.Items.Add(materialValue);
+            }
+
+            listBox1.SelectedIndex = 0;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Material material = (Material)listBox1.SelectedItem;
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            //Validations
+            if (listBox1.SelectedItem == null)
+            {
+               
+                MessageBox.Show("Please select a desktop material");
+               
+                
+                return;
+            }
+
+            MessageBox.Show(this.width.Text);
+            MessageBox.Show(this.length.Text);
+            MessageBox.Show(this.days.Text);
+            MessageBox.Show(this.drawers.Text);
+            DeskOrder myDeskOrder = new MegaEscarito.DeskOrder();
+            MessageBox.Show(listBox1.SelectedItem.ToString());
+            //myDeskOrder.SetMaterial(listBox1.SelectedItem);
+        }
+
+    }
+    public class DeskOrder
+    {
+        private Material material;
+
+        public DeskOrder()
+        {
+
+        }
+
+        public void SetMaterial(MegaEscarito.Material deskMaterial)
+        {
+            this.material = deskMaterial;
+            MessageBox.Show(deskMaterial.ToString());
+        }
+    }
+}
